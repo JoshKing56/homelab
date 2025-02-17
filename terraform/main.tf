d67b09b6f3ca7dcb07ea7d0ec9068ea79c5f2a61
Joshua King <joshua.king1@ahead.com>
2025-02-16 19:50:44 -0600

    Tf fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "test-ubuntu-vm" {
-   source = "./modules/vm/ubuntu"
-   ssh_key = "./id_ed25519.pub"
-   vm_name = "test-ubuntu"
+  source  = "./modules/vm/ubuntu"
+  ssh_key = "./id_ed25519.pub"
+  vm_name = "test-ubuntu"
 }
