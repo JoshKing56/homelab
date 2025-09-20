779e92400b3002e0f8ec1f578b19f97673aa2e4e
Joshua King <joshua.king1@ahead.com>
2025-09-20 02:04:38 -0500

    Proving claude wrong

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ module "vm" {
   vlan_tag       = each.value.vlan_tag != null ? each.value.vlan_tag : var.vlan_num
 
   bios               = each.value.bios
-  boot               = each.value.boot_device_order
+  boot_device_order   = each.value.boot_device_order
   qemu_agent_enabled = each.value.qemu_agent_enabled
   iso_file           = each.value.iso_file != null ? each.value.iso_file : var.iso_file
   os_type            = each.value.os_type
